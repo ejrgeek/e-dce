06bf7af5157514b273a23aa59d9a037780da8d00
Erlon Jr <ejrgeek@gmail.com>
2020-10-31 21:55:44 -0300

    feat(candidato): add app candidatos

---
+++ apps/candidato/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+def upload_foto(instance, filename):
+    return f'foto/{filename}'
+
+
+class Candidato(models.Model):
+    nome = models.CharField(max_length=255)
+    numero = models.PositiveIntegerField()
+    foto = models.ImageField(upload_to=upload_foto)
+
+    def __str__(self):
+        return f'{self.nome} - {self.numero}'
--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    'apps.candidato',
 ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
