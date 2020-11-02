e0274375e2e7e6697cb6757f703289a369778934
Erlon Jr <ejrgeek@gmail.com>
2020-11-02 00:08:15 -0300

    chore(settings): add aws in csp

--- core/settings.py
+++ core/settings.py
@@ -77,7 +77,8 @@ CSP_DEFAULT_SRC = (
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://code.jquery.com/jquery-3.3.1.slim.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
-    'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
+    'https://s3.amazonaws.com'
 )
 
 SECURE_HSTS_SECONDS = 2592000
